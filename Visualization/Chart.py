ccb624455fda9f44b6d70bb2abe75f4f95f98b21
Haßlauer, Jonas <jonas.hasslauer@student.fhws.de>
2022-05-01 16:15:55 +0200

    - Added Example Solution for input data (till DB is up) - First solution for line diagramm

--- Visualization/Chart.py
+++ Visualization/Chart.py
@@ -1,23 +1,70 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def makeLineDiagramm(dataForX, dataForY, title="", xlabel="", ylabel=""):
+    plt.plot(dataForX, dataForY)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+
+def makePieDiagramm():
+    pass
+
+
+def makeBarDiagramm():
+    pass
+
+
 class Chart:
+    chartType = ""
+    columnsPerFile = 0
+    linesPerFile = 0
 
-    columns = 0
-    lines = 0
+    def getDataFromSQL(self):
+        pass
 
-    def setColumns(self, columns:int) -> int:
+    def setSpecificLinesForChart(self, lines):
+        pass
+
+    def setSpecificColumnsForChart(self, columns):
+        pass
+
+    def setAmoutOfColumnsPerFile(self, columns: int) -> int:
+        """
+        :param columns: Represents the amount of selected columns in the given list
+        :return int:  Return - 1 if the amount of columns equals 0
+        """
         if columns == 0:
             return -1
-        self.columns = columns
+        self.columnsPerFile = columns
 
-    def getColumns(self):
-        pass
+    def getColumnsPerFile(self):
+        return self.columnsPerFile
 
-    def setLines(self, lines:int) -> int:
+    def setAmountOfLinesPerFile(self, lines: int) -> int:
+        """
+        :param lines: Represents the amount of selected lines in the given list
+        :return int:  Return - 1 if the amount of lines equals 0
+        """
         if lines == 0:
             return -1
-        self.columns = lines
+        self.linesPerFile = lines
 
-    def  getLines(self):
-        pass
+    def getLinesPerFile(self):
+        return self.linesPerFile
 
+    def setChartType(self, chartType: str) -> None:
+        """
+        :param chartType: The art of chart the user wants to get visualized (Linien-, Balken- oder Torte)
+        :return None
+        """
+        self.chartType = chartType
 
-
+    def getChartType(self) -> chartType:
+        """"
+        :return chartType: The art of chart the user wants to get visualized
+        """
+        return self.chartType
---
+++ Visualization/OrganizeDF.py
@@ -0,0 +1,26 @@
+from pandas import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def getDataFrameFromCSV(path: str) -> pd.DataFrame:
+    col_list = ['Gebiet', 'Umsatz', 'Status']
+    file = pd.read_csv(path, usecols=col_list)
+    df = pd.DataFrame(file)
+    return df
+
+
+def getColumnsFromCSVFile(file: pd.DataFrame) -> list:
+    return list(file.columns)
+
+
+class OrganizeDF:
+    """
+    Class for reorganizing the DF to extract data in the right way
+    """
+
+    pathToCSV = ""
+    dataframe = ""
+
+    def __init__(self, pathToCSV):
+        self.pathToCSV = pathToCSV
+        self.dataframe = getDataFrameFromCSV(self.pathToCSV)
