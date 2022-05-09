1fa2f6cc526b5d1cbed1dc3ebbae33e08a1bbb23
Haßlauer, Jonas <jonas.hasslauer@student.fhws.de>
2022-05-09 14:28:35 +0200

    - bugfixed diagramms - Added Classes and Inheritance for the cart classes

--- Visualization/Chart.py
+++ Visualization/Chart.py
@@ -1,28 +1,18 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 class Chart:
-    chartType = ""
     dataX = []
     dataY = []
     title = ""
     labelX = []
     labelY = []
-    PieLabel = []
-    PieLabelSize = []
 
-    def setPieLabel(self, labelPie: []):
-        self.PieLabel = labelPie
-
-    def getPieLabel(self) -> PieLabel:
-        return self.PieLabel
-
-    def setPieLabelSize(self, PieLabelSize: []):
-        self.PieLabelSize = PieLabelSize
-
-    def getPieLabelSize(self):
-        return self.PieLabelSize
+    def __init__(self, dataX, dataY):
+        self.dataX = dataX
+        self.dataY = dataY
 
     def setTitle(self, title: str):
         self.title = title
@@ -42,6 +32,74 @@ class Chart:
     def getDataY(self):
         return self.dataY
 
+
+class PieChart(Chart):
+    PieLabel = []
+    PieLabelSize = []
+
+    def __init__(self, PieLabel, PieLabelSize, dataX, dataY):
+        super().__init__(dataX, dataY)
+        self.PieLabel = PieLabel
+        self.PieLabelSize = PieLabelSize
+
+    def setPieLabel(self, labelPie: []):
+        self.PieLabel = labelPie
+
+    def getPieLabel(self):
+        return self.PieLabel
+
+    def setPieLabelSize(self, PieLabelSize: []):
+        self.PieLabelSize = PieLabelSize
+
+    def getPieLabelSize(self):
+        return self.PieLabelSize
+
+    def makePieChart(self):
+        fig1, ax1 = plt.subplots()
+        ax1.pie(self.PieLabelSize, labels=self.PieLabel, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.show()
+
+
+class BarChart(Chart):
+    labelX = []
+    labelY = []
+
+    def __init__(self, labelX: [], labelY: [], dataX, dataY):
+        self.labelX = labelX
+        self.labelY = labelY
+        super().__init__(dataX, dataY)
+
+    def setLabelX(self, labelX: str):
+        self.labelX = labelX
+
+    def getLabelX(self, labelX: str):
+        return self.labelX
+
+    def setLabelY(self, labelY: str):
+        self.labelY = labelY
+
+    def getLabelY(self, labelY: str):
+        return self.labelY
+
+    def makeBarChart(self):
+        plt.bar(x=self.dataX, height=self.dataY)
+        plt.title(self.title)
+        plt.xlabel(self.labelX)
+        plt.ylabel(self.labelY)
+        plt.show()
+
+
+class LineChart(Chart):
+    labelX = ""
+    labelY = ""
+
+    def __init__(self, labelX, labelY, dataX, dataY):
+        self.labelX = labelX
+        self.labelY = labelY
+        super().__init__(dataX, dataY)
+
     def setLabelX(self, labelX: str):
         self.labelX = labelX
 
@@ -57,37 +115,9 @@ class Chart:
     def getDataFromSQL(self):
         pass
 
-    def setChartType(self, chartType: str):
-        """
-        :param chartType: The art of chart the user wants to get visualized (Linien-, Balken- oder Torte)
-        :return None
-        """
-        self.chartType = chartType
-
-    def getChartType(self):
-        """"
-        :return chartType: The art of chart the user wants to get visualized
-        """
-        return self.chartType
-
-    def makeLineDiagramm(self):
+    def makeLineChart(self):
         plt.plot(self.dataX, self.dataY)
         plt.title(self.title)
         plt.xlabel(self.labelX)
         plt.ylabel(self.labelY)
         plt.show()
-
-    def makePieDiagramm(self):
-        fig1, ax1 = plt.subplots()
-        ax1.pie(self.PieLabelSize, labels=self.PieLabel, autopct='%1.1f%%',
-                shadow=True, startangle=90)
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        plt.show()
-        plt.savefig("PieDiagramm.png")
-
-    def makeBarDiagramm(self):
-        fig = plt.figure()
-        ax = fig.add_axes([0, 0, 1, 1])
-        ax.bar(self.dataX, self.dataY)
-        plt.show()
-        plt.savefig("BarDiagramm.png")
