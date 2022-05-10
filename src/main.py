5db050bd202a29dab5a9be361142a1b38f461672
Samim <samim.ayubi@student.fhws.de>
2022-05-10 15:02:18 +0200

    Logins wurden ausgetauscht unnötige methoden und html pages aus der vorherigen version wurden gelöscht. Upload methode wieder und es können keine / sprünge gemacht werden

--- src/main.py
+++ src/main.py
@@ -2,35 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 import pandas as pd
-import sqlite3 as sql
-import bcrypt
-
-connect = sql.connect("Datenbank/my_logins4.db")
-cursor = connect.cursor()
-CreateTable = "CREATE TABLE if not EXISTS Logins(username VARCHAR(10) PRIMARY KEY not null, firstname VARCHAR(100) not null, lastname VARCHAR (100) not null, birthday DATE not null, password BINARY(64) not null)";
-cursor.execute(CreateTable)
-connect.commit()
-
-
-def AddUSER(username, firstname, lastname, birthday, password):
-    con = sql.connect("Datenbank/my_logins4.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO Logins(username,firstname, lastname, birthday,password) VALUES (?,?,?,?,?)",
-                (username, firstname, lastname, birthday, password))
-    con.commit()
-    con.close()
-
-
-def check_User(username, password):
-    con = sql.connect("Datenbank/my_logins4.db")
-    cur = con.cursor()
-    cur.execute("SELECT username, password FROM Logins WHERE username=? and password=?", (username, password))
-    result = cur.fetchall()
-    if result:
-        return True
-    else:
-        return False
-
+import Datenbank
 
 app = Flask(__name__, template_folder="./templates")
 app.secret_key = "key"
@@ -55,10 +27,8 @@ def register():
         birthday = request.form.get("birthday")
         username = request.form.get("username")
         password = request.form.get("password")
-
-        AddUSER(username, firstname, lastname, birthday, password)
+        Datenbank.AddUSER(username, firstname, lastname, birthday, password)
         return redirect(url_for("index"))
-
     else:
         return render_template("register.html")
 
@@ -66,34 +36,33 @@ def register():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form['username']
+        password = request.form['password']
 
-        if check_User(username, password):
-            session["username"] = username
-        return redirect(url_for("uebersichtsseite"))
+        if Datenbank.check_User(username, password):
+            session['username'] = username
+        return redirect(url_for('uebersichtsseite'))
     else:
-        redirect(url_for("index"))
+        return redirect(url_for('index'))
 
 
 @app.route('/uebersichtsseite', methods=["POST", "GET"])
 def uebersichtsseite():
     if 'username' in session:
-        return render_template("uebersichtsseite.html", username=session['username'])
-        if request.method == 'POST':
-            if 'file' not in request.files:
-                return redirect(request.url)
+        return render_template('uebersichtsseite.html', username=session['username'])
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
             return redirect(request.url)
         if allowed(file.filename):
             filename = secure_filename(file.filename)
-        file.save(os.path.join(folder, filename))
-        return render_template('uebersichtsseite.html', Liste=os.listdir(
-            "Uploads"))  # auf der Übersichtsseite wird temporär nicht die datei angezeigt
+            file.save(os.path.join(folder, filename))
+            return render_template('uebersichtsseite.html', Liste=os.listdir("Uploads"))
+
     else:
         return "Username oder Passwort ist falsch!"
-
 
 list = pd.read_csv(os.getcwd() + "/Uploads" + "/Testdatei.csv", sep=";", decimal=".", header=0)
 # hier muss statt Testdatei.csv filename stehen die ausgewählt wurde bzw auch das temporäre anzeigen lassen
@@ -102,8 +71,10 @@ list.columns.values
 
 @app.route('/detailseite', methods=["POST", "GET"])
 def detailseite():
-    return render_template('detailseite.html', Liste=list.columns.values, bild="bewerbungen.png")
-
+    if 'username' in session:
+        return render_template('detailseite.html',username=session['username'],Liste=list.columns.values, bild="bewerbungen.png")
+    else:
+        return "Bitte einloggen!"
 
 @app.route("/logout", methods=["POST"])
 def logout():
