deab7d80508ba986554f273c52221d4492c80264
Samim <samim.ayubi@student.fhws.de>
2022-05-16 16:45:48 +0200

    Logins wurden ausgetauscht unnötige methoden und html pages aus der vorherigen version wurden gelöscht. Upload methode wieder und es können keine // sprünge gemacht werden. Hashmethode wurde eingefügt und passwort wird auch geprüft. Datenbank angelegt.

--- src/main.py
+++ src/main.py
@@ -27,6 +27,8 @@ def register():
         birthday = request.form.get("birthday")
         username = request.form.get("username")
         password = request.form.get("password")
+        #hashedpw = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        #password_hash = hashedpw.decode("utf-8")
         Datenbank.AddUSER(username, firstname, lastname, birthday, password)
         return redirect(url_for("index"))
     else:
@@ -38,18 +40,17 @@ def login():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-
-        if Datenbank.check_User(username, password):
+        if Datenbank.check_User(username, password) == True:
             session['username'] = username
+            Datenbank.changetimestamp()
         return redirect(url_for('uebersichtsseite'))
     else:
         return redirect(url_for('index'))
 
-
 @app.route('/uebersichtsseite', methods=["POST", "GET"])
 def uebersichtsseite():
     if 'username' in session:
-        return render_template('uebersichtsseite.html', username=session['username'])
+        return render_template('uebersichtsseite.html', username=session['username'], Liste=os.listdir("Uploads"))
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
@@ -62,7 +63,7 @@ def uebersichtsseite():
             return render_template('uebersichtsseite.html', Liste=os.listdir("Uploads"))
 
     else:
-        return "Username oder Passwort ist falsch!"
+        return redirect(url_for('login'))
 
 list = pd.read_csv(os.getcwd() + "/Uploads" + "/Testdatei.csv", sep=";", decimal=".", header=0)
 # hier muss statt Testdatei.csv filename stehen die ausgewählt wurde bzw auch das temporäre anzeigen lassen
@@ -72,14 +73,16 @@ list.columns.values
 @app.route('/detailseite', methods=["POST", "GET"])
 def detailseite():
     if 'username' in session:
-        return render_template('detailseite.html',username=session['username'],Liste=list.columns.values, bild="bewerbungen.png")
+        return render_template('detailseite.html',username=session['username'], Liste=list.columns.values, bild="bewerbungen.png")
     else:
-        return "Bitte einloggen!"
+        return redirect(url_for('login'))
 
 @app.route("/logout", methods=["POST"])
 def logout():
     session.clear()
     return redirect(url_for("index"))
+
+Datenbank.cleardata()
 
 
 if __name__ == "__main__":
