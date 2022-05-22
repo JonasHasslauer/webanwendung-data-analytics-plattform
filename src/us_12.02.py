6809ef98c6f95218c7debde475c0110533a65dcf
Haßlauer, Jonas <jonas.hasslauer@student.fhws.de>
2022-05-22 14:52:02 +0200

    reformatted

--- src/us_12.02.py
+++ src/us_12.02.py
@@ -1,22 +1,18 @@
 import pandas as pd
 
-
-
-#Test Dataframe mit zufälligen Daten
-df = pd.DataFrame({'Name' : ["Peter", "Karla", "Anne", "Nino", "Andrzej"],
+# Test Dataframe mit zufälligen Daten
+df = pd.DataFrame({'Name': ["Peter", "Karla", "Anne", "Nino", "Andrzej"],
                    'Alter': [34, 53, 16, 22, 61],
                    'Nationalität': ["deutsch", "schweizerisch", "deutsch", "italienisch", "polnisch"],
                    'Gehalt': [3400, 4000, 0, 2100, 2300]},
-                  index = ['ID-123', 'ID-462', 'ID-111', 'ID-997', 'ID-707'],
-                 columns = ['Name', 'Alter', 'Nationalität', 'Gehalt'])
+                  index=['ID-123', 'ID-462', 'ID-111', 'ID-997', 'ID-707'],
+                  columns=['Name', 'Alter', 'Nationalität', 'Gehalt'])
 
 
+# mit Hilfe der zeilenFiltern Methode können Zeilen ausgegeben werden, wenn sie einem bestimmten
+# Spaltenwert entsprechen/größer/kleiner sind
 
-
-#mit Hilfe der zeilenFiltern Methode können Zeilen ausgegeben werden, wenn sie einem bestimmten
-#Spaltenwert entsprechen/größer/kleiner sind
-
-def zeilenFiltern(df,spaltenname,wert,operator):
+def zeilenFiltern(df, spaltenname, wert, operator):
     """
 
     :param df: das zu bearbeitende DataFrame
@@ -25,24 +21,26 @@ def zeilenFiltern(df,spaltenname,wert,operator):
     :param operator: </>/==
     :return:gibt die gefilterten Zeilen als neues Dataframe aus
     """
-    if(operator=='>'):
+    if operator == '>':
         df_maske = df[spaltenname] > wert
         filtered_df = df[df_maske]
         return filtered_df
-    elif (operator == '<'):
+    elif operator == '<':
         df_maske = df[spaltenname] < wert
         filtered_df = df[df_maske]
         return filtered_df
-    elif (operator == '=='):
+    elif operator == '==':
         df_maske = df[spaltenname] == wert
         filtered_df = df[df_maske]
         return filtered_df
 
+
 spaltenFiltern_df = df[['Name', 'Alter', 'Gehalt']]
 print(spaltenFiltern_df, "\n")
 
-#mit der Funktion spaltenFiltern können einzelne oder mehere  Spalten ausgegeben werden
-def spaltenFiltern(df,liste):
+
+# mit der Funktion spaltenFiltern können einzelne oder mehere  Spalten ausgegeben werden
+def spaltenFiltern(df, liste):
     """
 
     :param df: zu bearbeitendes DataFrame
@@ -51,5 +49,7 @@ def spaltenFiltern(df,liste):
     """
     spaltenFiltern_df = df[liste]
     return spaltenFiltern_df
+
+
 """liste =['Name','Alter','Gehalt']
 print(spaltenFiltern(df,liste))"""
