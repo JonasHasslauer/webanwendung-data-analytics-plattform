73d839e5a6416673084f30e722f748a7824fbdb2
Haßlauer, Jonas <jonas.hasslauer@student.fhws.de>
2022-05-23 22:38:52 +0200

    added test files for Datenbank.py

---
+++ src/test_Datenbank.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestDatenbank(TestCase):
+    def test_create_login_table(self):
+        self.fail()
+
+    def test_get_all_usernames_from_database(self):
+        self.fail()
+
+    def test_check_if_user_exists(self):
+        self.fail()
+
+    def test_add_user(self):
+        self.fail()
+
+    def test_change_time_stamp(self):
+        self.fail()
+
+    def test_check_users(self):
+        self.fail()
+
+    def test_clear_data(self):
+        self.fail()
